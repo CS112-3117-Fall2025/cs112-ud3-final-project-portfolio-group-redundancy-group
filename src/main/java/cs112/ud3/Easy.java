7229b16d8e17837053ca36c353d444a94e683a72
careless-error <jfpetersonii@icloud.com>
2025-11-26 13:26:49 -0800

    easy medium hard link to main

--- src/main/java/cs112/ud3/Easy.java
+++ src/main/java/cs112/ud3/Easy.java
@@ -2,24 +2,17 @@ package cs112.ud3;
 
 public class Easy extends Difficulty{
     //CONSTRUCTORS
-    public Easy() {
-        super();
-    }
-
+    public Easy() { super("Easy", 10, 10, 10,10 ); }
     //Full constructor
     public Easy(String name, int width, int height, int mineTiles, int flags) {
         super(name, width, height, mineTiles, flags);
     }
-
     //Copy constructor
     public Easy(Easy o) {
         super(o);
     }
-
     //No setters or getters because they are inherited.
-
     //ABSTRACT METHODS IMPLEMENTED
-
     @Override
     public void difficultyChosen(Difficulty difficulty) {
         //placeholder until we can make this change the layout
